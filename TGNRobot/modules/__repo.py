8e261b807e1e23d885f6d9a650e67f9b489021ea
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-11-21 23:12:41 +0300

    Update __repo.py

--- TGNRobot/modules/__repo.py
+++ TGNRobot/modules/__repo.py
@@ -5,18 +5,18 @@ from pyrogram.types import *
 from TGNRobot.conf import get_str_key
 from TGNRobot import pbot
 
-REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/8fe203de66df070f238bf.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Pratheek](t.me/pratheek06) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @AnimeXWrld_Chat «««"
+REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/8fe203de66df070f238bf.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Pratheek](t.me/isIam07) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @TeamBORZ95 «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("⚡ ʀᴇᴘᴏꜱɪᴛᴏʀʏ🔥", url=f"https://pratheek06"),
-        InlineKeyboardButton(" ᴊᴏɪɴ 💫", url=f"https://t.me/AnimeXWrld_Chat"),
+        InlineKeyboardButton("⚡ ʀᴇᴘᴏꜱɪᴛᴏʀʏ🔥", url=f"https://isIam07"),
+        InlineKeyboardButton(" ᴊᴏɪɴ 💫", url=f"https://t.me/TeamBORZ95"),
       ],[
-        InlineKeyboardButton("ᴏᴡɴᴇʀ ❣️", url="https://t.me/pratheek06"),
-        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ⚡", url="https://t.me/AnimeXWrld_Chat"),
+        InlineKeyboardButton("ᴏᴡɴᴇʀ ❣️", url="https://t.me/isIam07"),
+        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ⚡", url="https://t.me/TeamBORZ95"),
       ],[
-        InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇꜱ ☑️", url="https://t.me/AnimeXWrld_Chat"),
-        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ➡️", url="https://t.me/DamnItsNikhil"),
+        InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇꜱ ☑️", url="https://t.me/isIam07"),
+        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ ➡️", url="https://t.me/isIam07"),
       ]]
     )
   
