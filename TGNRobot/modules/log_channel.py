8538fa73a5a28df8a978bcef53a93f4f62987826
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-11-22 18:17:04 +0300

    Update log_channel.py

--- TGNRobot/modules/log_channel.py
+++ TGNRobot/modules/log_channel.py
@@ -193,14 +193,12 @@ if is_module_loaded(FILENAME):
         return "No log channel is set for this group!"
 
     __help__ = """
-*Admins only:*
- ❍ /logchannel*:* get log channel info
- ❍ /setlog*:* set the log channel.
- ❍ /unsetlog*:* unset the log channel.
-Setting the log channel is done by:
-❍ adding the bot to the desired channel (as an admin!)
-❍ sending /setlog in the channel
-❍ forwarding the /setlog to the group
+* Только администраторы: *
+ ❍ /logchannel *:* получить информацию о канале журнала
+ ❍ /setlog *:* установить канал журнала.
+ ❍ /unsetlog *:* отключить канал журнала.
+Настройка канала журнала выполняется:
+❍ добавление бота к желаемому чату
 """
 
     __mod_name__ = "Channel"
