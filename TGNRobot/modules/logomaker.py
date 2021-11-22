dcc7352756b7069fb5851a141d3b27256c5beec1
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-11-22 08:59:11 +0300

    Update logomaker.py

--- TGNRobot/modules/logomaker.py
+++ TGNRobot/modules/logomaker.py
@@ -80,7 +80,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Gotfathersupport, {e}')
+   await event.reply(f'Error Report @isIam07, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
